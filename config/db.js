d0606b80f955f563d90e7089fcd9995642bdc6be
Marcel07-Mpy <114915805+Marcel07-Mpy@users.noreply.github.com>
2026-09-21 00:26:05 +0100

    db: support Neon custom prefix URL

--- config/db.js
+++ config/db.js
@@ -13,6 +13,7 @@ const connectionString =
   process.env.POSTGRES_URL ||
   process.env.POSTGRES_PRISMA_URL ||
   process.env.NEON_DATABASE_URL ||
+  process.env.NEON_URL ||
   process.env.DATABASE_URL_UNPOOLED ||
   null;
 
